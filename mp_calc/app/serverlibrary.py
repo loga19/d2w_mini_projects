36d61c30ecab4e10b3f8c5e8cd6e183f5bb24202
student <team3641897@asn2849304>
2024-11-12 04:49:11 +0000

    Insert Stack Class

--- mp_calc/app/serverlibrary.py
+++ mp_calc/app/serverlibrary.py
@@ -4,6 +4,27 @@ def mergesort(array, byfunc=None):
   pass
 
 class Stack:
+  class Stack:
+  def __init__(self, first=None):
+      self.stackls = list()
+      if first != None:
+          self.push(first)
+  def push(self, thingy):
+      self.stackls.append(thingy)
+  def pop(self):
+      if not self.is_empty:
+          return self.stackls.pop()
+      return None
+  def peek(self):
+      if not self.is_empty:
+          return self.stackls[-1]
+      return None
+  @property
+  def is_empty(self):
+      return len(self.stackls)==0
+  @property
+  def size(self):
+    return len(self.stackls)
   pass
 
 class EvaluateExpression:
